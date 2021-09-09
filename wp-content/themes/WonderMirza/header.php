04d5460da0d6133c45e1d384c7909cb52339aa85
rihan <mohd.ansari@naukri.com>
2021-09-09 20:09:25 +0530

    [MAIN] add custom class in header file for checking....

--- wp-content/themes/WonderMirza/header.php
+++ wp-content/themes/WonderMirza/header.php
@@ -46,7 +46,7 @@ if (!defined('ABSPATH')) {
                             <h4><?php echo get_field('tagline', 'option'); ?></h4>
                         </div>
                     </div>
-                    <ul class="main-menu">
+                    <ul class="main-menu main-menu-class">
                         <?php
                         $primaryNav = wp_get_nav_menu_items(2);
                         foreach ($primaryNav as $key => $nav) {
